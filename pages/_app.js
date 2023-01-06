902808b9ca7d98ed0d135f20259499da779ae633
Toba-dean <tobaogundimu@gmail.com>
2023-01-06 19:02:06 +0100

    first commit

--- pages/_app.js
+++ pages/_app.js
@@ -1,5 +1,24 @@
-import '../styles/globals.css'
+import { createGlobalStyle } from "styled-components";
+import { useEffect } from "react";
+
+const GlobalStyle = createGlobalStyle`
+  *, *::before, *::after {
+    margin: 0;
+    padding: 0;
+    box-sizing: border-box;
+  }
+`;
 
 export default function App({ Component, pageProps }) {
-  return <Component {...pageProps} />
+
+  useEffect(() => {
+    document.title = "Star Wars"
+  })
+
+  return (
+    <>
+      <GlobalStyle />
+      <Component {...pageProps} />
+    </>
+  )
 }
